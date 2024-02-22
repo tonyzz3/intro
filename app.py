e881cbf597b5c370e1a8daa783c7400199c81b08
tonyzz3 <160858139+tonyzz3@users.noreply.github.com>
2024-02-22 07:40:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,17 @@ st.write("si te copio por que no leo :(")
 image = Image.open('stick.jpg')
 
 st.image(image, caption= 'sisoy')
+
+texto = st.text_input('we are the world', 'we are the children')
+st.write ('we are the one who makes a brighter day', texto)
+
+st.subheader("so let´s start giving")
+
+coll, coll2 = st.colums(2)
+
+with coll: 
+  st.subheader ("Martha tiene un marca pasos")
+  st.write ("Sufre mamón, devuelveme a mi chica") 
+  resp= st. checkbox("Mamma mia, here we go again")
+  if resp:
+    st.write("AJU NICE!")
