7fbba39d0ac43923b208f67f5ad065a1372d21e8
tonyzz3 <160858139+tonyzz3@users.noreply.github.com>
2024-02-22 07:20:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st 
 from PIL import Image
 
-st.title("Mamawebo")
+st.title("Mi primera App!")
+
+st.header("No me gusta programar uwu")
+st.write("si te copio por que no leo :(")
+image = Image.open('')
